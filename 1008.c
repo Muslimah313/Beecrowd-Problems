e48a16683ff54f5b7268a982f17ecda7e12f7f72
Muslimah313 <ummati300@gmail.com>
2025-09-02 06:06:39 +0600

    Update 1008.c

--- 1008.c
+++ 1008.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main() {
+    
+        int number,hours;
+    double vh,salary;
+
+    scanf("%d %d %lf",&number,&hours,&vh);
+
+    salary = hours*vh;
+
+    printf("NUMBER = %d\n", number);
+    printf("SALARY = U$ %.2lf\n", salary);
+
+    return 0;
+}
+
 
