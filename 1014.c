534419929909ea7ac118f29c5fd11590c8c017e1
Muslimah313 <ummati300@gmail.com>
2025-09-02 06:26:30 +0600

    Add files via upload

---
+++ 1014.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   int X;
+   scanf("%d",&X);
+    double Y,average;
+    scanf("%lf",&Y);
+    average = X/Y;
+    printf("%.3lf km/l",average);
+
+    return 0;
+}
+
