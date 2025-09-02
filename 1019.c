9ebf0ab4acaafd8e7a512d260d323d30ced41228
Muslimah313 <ummati300@gmail.com>
2025-09-02 06:29:05 +0600

    Add files via upload

---
+++ 1019.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+
+      int hour=(N/3600);
+
+   int min=(N%3600)/60;
+
+    int sec=(N%3600)%60;
+
+    printf("%d:%d:%d\n", hour,min,sec);
+
+    return 0;
+}
