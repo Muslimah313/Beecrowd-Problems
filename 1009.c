cd6b94f99626ca90e4ac05573b4a7d2a2265559c
Muslimah313 <ummati300@gmail.com>
2025-09-02 06:14:02 +0600

    Update 1009.c

--- 1009.c
+++ 1009.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    double salary,tSales,fSalary;
+
+    scanf("%s %lf %lf",name,&salary,&tSales);
+
+    fSalary = salary+(tSales*0.15);
+
+    printf("TOTAL = R$ %.2lf\n",fSalary);
+
+    return 0;
+}
 
